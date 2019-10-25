4f278cf501382d2196b5a9a29d3b12c5e3319536
4ound <lnfedorov18@gmail.com>
2019-10-26 02:46:53 +0300

    fibonacci method initial

--- lab1/unittests.py
+++ lab1/unittests.py
@@ -1,6 +1,6 @@
 import unittest
 from lab1 import variant as var
-from lab1.methods import dichotomy
+from lab1.methods import dichotomy, fibonacci
 
 
 class TestDichotomyMethod(unittest.TestCase):
@@ -24,3 +24,26 @@ class TestDichotomyMethod(unittest.TestCase):
         f = var.Variant8.f
         answer = var.Variant8.answer
         self.assertTrue(dichotomy(f, a, b) == answer)
+
+
+class TestFibonacciMethod(unittest.TestCase):
+    def test_3d_variant(self):
+        a = var.Variant3.a
+        b = var.Variant3.b
+        f = var.Variant3.f
+        answer = var.Variant3.answer
+        self.assertTrue(fibonacci(f, a, b) == answer)
+
+    def test_7th_variant(self):
+        a = var.Variant7.a
+        b = var.Variant7.b
+        f = var.Variant7.f
+        answer = var.Variant7.answer
+        self.assertTrue(fibonacci(f, a, b) == answer)
+
+    def test_8th_variant(self):
+        a = var.Variant8.a
+        b = var.Variant8.b
+        f = var.Variant8.f
+        answer = var.Variant8.answer
+        self.assertTrue(fibonacci(f, a, b) == answer)
