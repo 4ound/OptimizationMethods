7e85ceff1b306fc1bb7d9413958d66bca999b38f
Konstantin Mosunov <kmosunoff@mail.ru>
2019-10-26 02:49:12 +0300

    added gradient descent

--- lab1/methods.py
+++ lab1/methods.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from answer import Answer
 from constants import EPS
 from lab1.variant import Variant3 as Var
+from math import sqrt
 
 DELTA = EPS / Decimal(2)
 
@@ -33,6 +34,56 @@ def fibonacci_n(n):
             ((Decimal("1") + Decimal("5").sqrt()) / Decimal("2")) ** Decimal(n) -
             ((Decimal("1") - Decimal("5").sqrt()) / Decimal("2")) ** Decimal(n)
     )
+
+
+def gradient_descent():
+
+    def f(x):
+        return (x[1] - x[0]) ** 2 + 100 * (1 - x[0]) ** 2
+
+    def f_x0(x):
+        return 202 * x[0] - 2 * x[1] - 200
+
+    def f_x1(x):
+        return 2 * x[1] - 2 * x[0]
+
+    eps_f = Decimal(1e-2)
+    eps_x = (Decimal(1e-2), Decimal(1e-2))
+    X = [Decimal(100), Decimal(100)]
+    iterations = 0
+
+    while True:
+        iterations += 1
+        gradient = [f_x0(X), f_x1(X)]
+        gradient_len = Decimal(sqrt(sum(x_i ** 2 for x_i in gradient)))
+        S = []
+        for x_i in gradient:
+            S.append(x_i / gradient_len)
+
+        def g(lam):
+            args = [X[i] - lam * S[i] for i in range(len(X))]
+            return f(args)
+
+        l = dichotomy(g, Decimal(-200), Decimal(200)).x
+
+        X_next = []
+        for i in range(len(X)):
+            X_next.append(X[i] - l * S[i])
+
+        flag = True
+        for i in range(len(X)):
+            if X_next[i] - X[i] > eps_x[i]:
+                flag = False
+                break
+
+        if abs(f(X_next) - f(X)) < eps_f or flag:
+            print(X_next)
+            print(f(X_next))
+            print(iterations)
+            break
+
+        X = X_next
+        print(f(X))
 
 
 def main():
